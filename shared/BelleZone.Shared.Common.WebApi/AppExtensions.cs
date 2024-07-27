d572599465d567b1b4ce10b3f4cd48c06f70de1a
Sanjyot Agureddy <sanjyot.agureddy4@gmail.com>
2024-07-27 18:50:31 +0530

    Added Global Error handling

---
+++ shared/BelleZone.Shared.Common.WebApi/AppExtensions.cs
@@ -0,0 +1,13 @@
+﻿using BelleZone.Shared.Common.WebApi.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace BelleZone.Shared.Common.WebApi;
+
+public static class AppExtensions
+{
+  public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder app)
+  {
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+    return app;
+  }
+}
