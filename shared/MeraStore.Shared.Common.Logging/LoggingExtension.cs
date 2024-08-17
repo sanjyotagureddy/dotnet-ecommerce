981b23f96ef6b0b29eaa90de1fbeaef6d037b72a
Sanjyot Agureddy <sanjyot.agureddy4@gmail.com>
2024-08-17 05:58:02 +0530

    Sets different loggings

--- shared/MeraStore.Shared.Common.Logging/LoggingExtension.cs
+++ shared/MeraStore.Shared.Common.Logging/LoggingExtension.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Hosting;
 
 using Serilog;
+using Serilog.Filters;
 using Serilog.Sinks.Elasticsearch;
 
 
@@ -13,47 +14,49 @@ public static class LoggingExtension
   {
     return hostBuilder.UseSerilog((context, configuration) =>
     {
-      configuration.Enrich.FromLogContext()
-        .ReadFrom.Configuration(context.Configuration)
-        .Enrich.WithProperty("app-name", appName)
-        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName ?? "Development")
-        .Enrich.WithAssemblyName()
-        .Enrich.WithMachineName()
-        .WriteTo.Console(outputTemplate: $"[{{SourceContext}}-{appName}-{{MachineName}}]{{NewLine}}{{Timestamp:ddMMyyyy HH:mm:ss}} {{Level:u4}}] {{Message:lj}}{{NewLine}}{{Exception}}")
-        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
-        {
-
-          IndexFormat =
-            $"app-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.Now:yyyy-MM}",
-          AutoRegisterTemplate = true,
-          NumberOfShards = 2,
-          NumberOfReplicas = 1
-        });
+      // Base configuration for all logs
+      configuration
+          .ReadFrom.Configuration(context.Configuration)
+          .Enrich.WithProperty("app-name", appName)
+          .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName ?? "Development")
+          .Enrich.WithAssemblyName()
+          .Enrich.WithMachineName()
+          .WriteTo.Console(outputTemplate: $"[{{SourceContext}}-{appName}-{{MachineName}}]{{NewLine}}{{Timestamp:ddMMyyyy HH:mm:ss}} {{Level:u4}}] {{Message:lj}}{{NewLine}}{{Exception}}");
 
       // EF Core logs to a different index
       configuration.WriteTo.Logger(logger => logger
-        .Filter.ByIncludingOnly(logEvent => logEvent.Properties.TryGetValue("SourceContext", out var sourceContext)
-                                            && sourceContext.ToString().StartsWith("\"Microsoft.EntityFrameworkCore\""))
-        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
-        {
-          IndexFormat = $"efcore-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.Now:yyyy-MM}",
-          AutoRegisterTemplate = true,
-          NumberOfShards = 2,
-          NumberOfReplicas = 1
-        }));
+        .Filter.ByIncludingOnly(Matching.FromSource("Microsoft.EntityFrameworkCore"))
+          .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+          {
+            IndexFormat = $"efcore-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.Now:yyyy-MM}",
+            AutoRegisterTemplate = true,
+            NumberOfShards = 2,
+            NumberOfReplicas = 1
+          }));
 
-      // ASP.NET Core Logs
+      // ASP.NET Core logs to a different index
       configuration.WriteTo.Logger(lc => lc
-        .Filter.ByIncludingOnly(logEvent => logEvent.Properties.TryGetValue("SourceContext", out var sourceContext)
-                                            && sourceContext.ToString().StartsWith("\"Microsoft.AspNetCore\""))
-        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
-        {
-          IndexFormat = $"aspnetcore-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.Now:yyyy-MM}",
-          AutoRegisterTemplate = true,
-          NumberOfShards = 2,
-          NumberOfReplicas = 1
-        }));
+        .Filter.ByIncludingOnly(Matching.FromSource("Microsoft.AspNetCore"))
+          .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+          {
+            IndexFormat = $"aspnetcore-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.Now:yyyy-MM}",
+            AutoRegisterTemplate = true,
+            NumberOfShards = 2,
+            NumberOfReplicas = 1
+          }));
 
+      // General application logs to a different index
+      configuration.WriteTo
+        .Logger(lc => lc
+          .Filter.ByExcluding(Matching.FromSource("Microsoft"))
+          .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+
+          {
+            IndexFormat = $"app-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.Now:yyyy-MM}",
+            AutoRegisterTemplate = true,
+            NumberOfShards = 2,
+            NumberOfReplicas = 1
+          }));
     });
   }
 }
--- src/Product/MeraStore.Product.Api/Program.cs
+++ src/Product/MeraStore.Product.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using MeraStore.Shared.Common.Logging;
 using MeraStore.Shared.Common.WebApi;
 using MeraStore.User.Shared.Common;
@@ -5,11 +7,18 @@ using MeraStore.User.Shared.Common;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
-builder.Host.UseLogging(Constants.ServiceIdentifiers.User);
+builder.Host.UseLogging(Constants.ServiceIdentifiers.Product);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+  options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+  options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+  options.JsonSerializerOptions.WriteIndented = true;
+  options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
